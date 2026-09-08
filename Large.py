9ddf67aca1b82a11bedffd0138cef90f4a60d18f
manjunath-cod <manjunath.s.hugar0@gmail.com>
2026-09-08 10:41:31 +0530

    Add comparison for largest of two numbers

---
+++ Large.py
@@ -0,0 +1,7 @@
+A = int(input("Enter the first Number: ")
+B = int(input("Enter the first Number: ")
+if(A>B):
+        print("A is the largest Number")
+else:
+       print("B is the largest Number")
+       
